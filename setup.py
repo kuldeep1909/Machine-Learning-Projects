c06fb327f4b301d3a7077ad4d82aa9da528a5e2a
“[firstname <malviyakuldeep38@gmail.com>
2024-09-08 18:10:50 +0530

    First setup commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+# this file is most important for the project because by this our whole 
+# project can be converted into a pypi packages and we can post this
+
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT= '-e .'
+
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This funciton will return the list of requirements 
+    
+    '''
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readline()
+        requirements= [req.replace('\n',"")for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name= 'mlproject',
+    version='0.0.1',
+    author='Kuldeep Malviya',
+    author_email='malviyakuldeep38@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+# this file is used to make our packages of the project
